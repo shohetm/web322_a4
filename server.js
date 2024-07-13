3ed51b5622a29cccebc9614906983984a863a709
Mike Shohet <mshohet@myseneca.ca>
2024-07-13 15:26:48 -0400

    server.js update

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const legoData = require('./modules/legoSets');
